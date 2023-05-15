d714d40401cdc75476be7cbc7a1be952eb84f20b
Sudeep <sudeep7217@gmail.com>
2023-05-16 00:48:47 +0530

    small

--- includes/signup.php
+++ includes/signup.php
@@ -16,7 +16,7 @@ $query->execute();
 $lastInsertId = $dbh->lastInsertId();
 if($lastInsertId)
 {
-$_SESSION['msg']="You are Scuccessfully registered. Now you can login ";
+$_SESSION['msg']="Registered Successfully.";
 header('location:thankyou.php');
 }
 else 
