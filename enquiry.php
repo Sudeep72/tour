b75864a938b53961082d8d3ccb90d69a8abad5c4
Sudeep <sudeep7217@gmail.com>
2023-05-16 00:53:27 +0530

    minor

--- enquiry.php
+++ enquiry.php
@@ -53,6 +53,11 @@ $error="Something went wrong. Please try again";
 	<script>
 		 new WOW().init();
 	</script>
+	<script>
+  setTimeout(function() {
+    document.querySelector('#myDiv').classList.add('hide');
+  }, 3000);
+</script>
   <style>
 		.errorWrap {
     padding: 10px;
@@ -87,8 +92,8 @@ $error="Something went wrong. Please try again";
 	<div class="container">
 		<h3 class="wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">Enquiry Form Password</h3>
 		<form name="enquiry" method="post">
-		 <?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
-				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+		 <?php if($error){?><div id="myDiv" class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
+				else if($msg){?><div id="myDiv" class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
 	<p style="width: 350px;">
 		
 			<b>Full name</b>  <input type="text" name="fname" class="form-control" id="fname" placeholder="Full Name" required="">
